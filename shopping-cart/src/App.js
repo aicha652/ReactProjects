a8cc630246883c62c6c5fb6653083c67776e2281
aicha652 <aichaazlf652@gmail.com>
2024-07-06 17:40:00 +0200

    Shopping Cart Challenge

---
+++ shopping-cart/src/App.js
@@ -0,0 +1,52 @@
+import { useState } from 'react';
+
+const initialProducts = [{
+  id: 0,
+  name: 'Baklava',
+  count: 1,
+}, {
+  id: 1,
+  name: 'Cheese',
+  count: 5,
+}, {
+  id: 2,
+  name: 'Spaghetti',
+  count: 2,
+}];
+
+export default function ShoppingCart() {
+  const [
+    products,
+    setProducts
+  ] = useState(initialProducts)
+
+  function handleIncreaseClick(productId) {
+    const newProducts = products.map((product) => {
+      if (product.id === productId) {
+        let newProduct = {...product, count: product.count + 1}
+        return newProduct
+      }
+      else{
+        return product
+      }
+    })
+    setProducts(newProducts)
+  }
+
+  return (
+    <ul>
+      {products.map(product => (
+        <li key={product.id}>
+          {product.name}
+          {' '}
+          (<b>{product.count}</b>)
+          <button onClick={() => {
+            handleIncreaseClick(product.id);
+          }}>
+            +
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+}
