9391d515d177f96f1fd527b8a9a9aac6c0a28487
aicha652 <aichaazlf652@gmail.com>
2024-07-12 14:37:12 +0200

    using usEffect with useRef

--- use-ref/src/App.js
+++ use-ref/src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
-import { useRef } from "react"
+import { useRef, useEffect } from "react"
 
 function App() {
   let number = useRef()
   function handleButtonClick() {
     number.current.value = Number(number.current.value) + 1
   }
+
+  useEffect(() =>{
+    number.current.focus()
+  })
+
+  /*function handleButtonClick() {
+    number.current.focus()
+  }*/
+
   return (
     <div>
       <input ref={number}></input>
