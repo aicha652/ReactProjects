7e1624d389e7c6d9b2f996e51df7aeb2d1a6bbf6
aicha652 <aichaazlf652@gmail.com>
2024-07-22 12:47:14 +0200

    Middleware

--- redux-project/src/App.js
+++ redux-project/src/App.js
@@ -1,6 +1,9 @@
 const redux = require("redux")
 const createStore = redux.createStore
 const combineRducers = redux.combineReducers
+const reduxLogger =  require("redux-logger")
+const applyMiddleware = redux.applyMiddleware
+const logger = reduxLogger.createLogger()
 
 
 const BUY_CAKE = "BUY_CAKE"
@@ -79,11 +82,13 @@ const rootReducer = combineRducers({
   iceCream: icecreamReducer
 })
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, applyMiddleware(logger))
 console.log("initial state", store.getState())
-const unsubscribe = store.subscribe(() => {
+/* const unsubscribe = store.subscribe(() => {
   console.log("Updates state", store.getState())
-})
+}) */
+
+const unsubscribe = store.subscribe(() => {})
 store.dispatch(buyCake())
 store.dispatch(buyCake())
 store.dispatch(buyCake())
